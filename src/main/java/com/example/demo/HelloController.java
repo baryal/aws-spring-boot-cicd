c3a906d483d8774807d32c9e12724fd3a48b708d
Babu Aryal <babu.aryal03@gmail.com>
2020-07-01 14:33:49 -0500

    Add query parameter.

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +12,7 @@ public class HelloController {
 
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!!";
+    public String sayHello(@RequestParam("name") String name) {
+        return "Hello " + name;
     }
 }
